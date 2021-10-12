48df07aa4a0e193c2c652578cd2d4f18728309d1
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-10-12 10:57:03 -0300

    service initial tests done and compoents test started

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -13,6 +13,16 @@ import {setupCourses} from '../common/setup-test-data';
 
 describe('CoursesCardListComponent', () => {
 
+  let component: CoursesCardListComponent;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule]
+    })
+    .compileComponents()
+    .then(() => {
+
+    });
+  })
 
   it("should create the component", () => {
 
--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { COURSES } from "../../../../server/db-data";
+import { COURSES, findLessonsForCourse } from "../../../../server/db-data";
+import { Course } from "../model/course";
+import { HttpErrorResponse } from "@angular/common/http";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService;
@@ -37,5 +39,90 @@ describe("CoursesService", () => {
       // flush serve para determinar os valores de retorno do metodo
       // de uma requisicao http, serve tanto para retornos com ou sem erros
       req.flush({payload: Object.values(COURSES)});
+  });
+
+  it('should find a course by id', () => {
+    coursesService.findCourseById(12)
+      .subscribe(courses => {
+        expect(courses).toBeTruthy('No courses returned');
+        expect(courses.id).toBe(12,"incorrect course");
+      });
+
+      const req = httpTestingControlller.expectOne('/api/courses/12');
+
+      expect(req.request.method).toEqual("GET");
+
+      // flush serve para determinar os valores de retorno do metodo
+      // de uma requisicao http, serve tanto para retornos com ou sem erros
+      req.flush(COURSES[12]);
+  });
+
+  it('should save the course data', () => {
+    const changes : Partial<Course> = {titles:{description: 'Testing Course'}};
+
+    coursesService.saveCourse(12,changes)
+    .subscribe(course => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingControlller.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.body.titles.description)
+      .toEqual(changes.titles.description);
+
+    req.flush({
+      ...COURSES[12],
+      ...changes
+    })
+  });
+
+  it('should five an error if save course fails', () => {
+    const changes : Partial<Course> = {titles:{description: 'Testing Course'}};
+
+    coursesService.saveCourse(12, changes)
+      .subscribe(
+        () => fail("the save course operation failed"),
+
+        (error: HttpErrorResponse) => {
+          expect(error.status).toBe(500);
+        }
+      );
+
+    const req = httpTestingControlller.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual("PUT");
+
+    req.flush('Save course fail', 
+      {status: 500, statusText: 'Internal Server Error'});
+
+  });
+
+  it('should find a list os lessons', () => {
+
+      coursesService.findLessons(12)
+      .subscribe(lessons => {
+        expect(lessons).toBeTruthy();
+        expect(lessons.length).toBe(3);
+      });
+
+      const req = httpTestingControlller.expectOne(
+        req => req.url == '/api/lessons');
+
+        expect(req.request.method).toEqual("GET");
+
+        expect(req.request.params.get("courseId")).toEqual("12");
+        expect(req.request.params.get("filter")).toEqual("");
+        expect(req.request.params.get("sortOrder")).toEqual("asc");
+        expect(req.request.params.get("pageNumber")).toEqual("0");
+        expect(req.request.params.get("pageSize")).toEqual("3");
+
+        req.flush({
+          payload: findLessonsForCourse(12).slice(0, 3)
+        });
+  });
+
+  afterEach(() => {
+    httpTestingControlller.verify();
   });
 })
